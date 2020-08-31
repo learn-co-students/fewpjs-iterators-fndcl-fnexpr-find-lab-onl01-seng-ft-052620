722ed02b10ef1ec604a20d6f10ba363ca4cb9112
dwandrew <danwandrew@gmail.com>
2020-08-31 11:26:46 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(tester) {
+  let firstYear = tester.find (function (s) 
+  {return  s.result === 'W' }
+  )
+  return !!firstYear ? firstYear.year : undefined
 }
