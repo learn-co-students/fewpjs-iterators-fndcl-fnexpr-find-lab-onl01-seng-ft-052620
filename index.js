4a9764a65e49ba36b19c9c6e40b4bb8922dad0c0
jessegan <jesse13579@gmail.com>
2020-09-03 16:32:06 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let res = arr.find(function(obj){
+    return obj.result === "W"
+  })
+
+  if (res){
+    return res.year
+  } else {
+    return undefined
+  }
+
+}
