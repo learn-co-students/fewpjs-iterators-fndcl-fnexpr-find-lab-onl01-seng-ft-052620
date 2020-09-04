e8fbdbd7e1597aadf7d3649d1d28ac93c735e552
KaelinAlexander <kaelin.alexander@gmail.com>
2020-09-04 11:31:36 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let win = array.find (function(s) {return s.result === "W" })
+  if (win != undefined) {
+    return win.year
+  } else {
+    undefined
+  }
+}
