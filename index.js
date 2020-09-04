5c9e83a0e9d9c3f812d65c2bd85ebb6ac1c46903
sean-slaughter <sthompson2822@gmail.com>
2020-09-04 17:11:14 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record_array){
+  let win = record_array.find(function(record){
+    return record["result"] === 'W';
+  })
+  if(win){
+    return win["year"];
+  }
+}
