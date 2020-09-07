59caf956bc889f1993b53d8def10854f62e4223f
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-09-07 12:19:56 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(obj) {
+  const winYear = obj.find( y => y.result === "W")
+  return winYear ? winYear.year : undefined
+}
