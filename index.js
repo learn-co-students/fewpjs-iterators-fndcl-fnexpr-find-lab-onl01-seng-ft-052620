3cc7e648cae8cbddb23c5f6a76af57b10364b189
mspcity <codepark2019@gmail.com>
2020-09-06 22:07:00 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  const win = array.find((obj) => {return obj.result == "W"})
+  if(win) {
+    return win.year
+  }else{
+    return undefined
+  }
+}
